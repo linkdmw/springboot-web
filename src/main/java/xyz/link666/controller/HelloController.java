200d2bc76e94931de221867aeb8f00ea484665ed
link <cxwl.h@qq.com>
2020-08-03 17:53:13 +0800

    我特么直接666

--- src/main/java/xyz/link666/controller/HelloController.java
+++ src/main/java/xyz/link666/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @RequestMapping("hello")
     @ResponseBody
     public String  hello() {
-        return "Hello mother fucker";
+        return "Hello mother fucker666";
     }
 }
